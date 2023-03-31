80b62ee7de738906c0e8979a418a4981943cad63
Qurban-93 <qurban231293@gmail.com>
2023-04-01 00:36:25 +0400

    CourseEditCreate

--- MVCProject/Areas/Panel/Controllers/CourseController.cs
+++ MVCProject/Areas/Panel/Controllers/CourseController.cs
@@ -34,6 +34,11 @@ namespace MVCProject.Areas.Panel.Controllers
         {
             if (courseCreateVM == null) return NotFound();
             if (!ModelState.IsValid) return View();
+            if(_appContext.Courses.Any(c=>c.CourseName.Trim().ToLower() == courseCreateVM.CourseName.Trim().ToLower()))
+            {
+                ModelState.AddModelError("CourseName", "Eyni adli kurs movcuddur!");
+                return View(courseCreateVM);
+            }
             if (DateTime.Now > courseCreateVM.StartDate)
             {
                 ModelState.AddModelError("StartDate", "Tarix kecmish ola bilmez !");
@@ -52,7 +57,7 @@ namespace MVCProject.Areas.Panel.Controllers
             }
 
             Course newCourse = new Course();
-            newCourse.ImageUrl = courseCreateVM.Photo.SaveImage(_env,"img","course");
+            newCourse.ImageUrl = courseCreateVM.Photo.SaveImage(_env, "img", "course");
             newCourse.CourseName = courseCreateVM.CourseName;
             newCourse.AboutCourse = courseCreateVM.AboutCourse;
             newCourse.Duration = courseCreateVM.Duration;
@@ -64,11 +69,109 @@ namespace MVCProject.Areas.Panel.Controllers
             newCourse.Description = courseCreateVM.Description;
             newCourse.HowToApply = courseCreateVM.HowToApply;
             newCourse.SkillLevel = courseCreateVM.SkillLevel;
-            newCourse.Language= courseCreateVM.Language;
-            ViewBag.Succesful = "Course Added !";
-            
+            newCourse.Language = courseCreateVM.Language;
+            TempData["Added"] = "ok";
+
             _appContext.Courses.Add(newCourse);
             _appContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            Course? course = _appContext.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null) return NotFound();
+
+
+            CourseEditVM courseEditVM = new CourseEditVM();
+            courseEditVM.CourseName = course.CourseName;
+            courseEditVM.AboutCourse = course.AboutCourse;
+            courseEditVM.Duration = course.Duration;
+            courseEditVM.StartDate = course.StartDate;
+            courseEditVM.CoursePrice = course.CoursePrice;
+            courseEditVM.ClassDuration = course.ClassDuration;
+            courseEditVM.StudentsCount = course.StudentsCount;
+            courseEditVM.Certification = course.Certification;
+            courseEditVM.Description = course.Description;
+            courseEditVM.HowToApply = course.HowToApply;
+            courseEditVM.SkillLevel = course.SkillLevel;
+            courseEditVM.Language = course.Language;
+            courseEditVM.ImageUrl = course.ImageUrl;
+
+
+            return View(courseEditVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id, CourseEditVM courseEditVM)
+        {
+            if (id == null || id == 0) return NotFound();
+            Course? course = _appContext.Courses.FirstOrDefault(c => c.Id == id);
+
+            if (!ModelState.IsValid)
+            {
+                courseEditVM.ImageUrl = course.ImageUrl;
+                return View(courseEditVM);
+            }
+
+           
+            if (course == null) return NotFound();
+
+            if(_appContext.Courses.Any(c=>c.CourseName.Trim().ToLower() == courseEditVM.CourseName.Trim().ToLower() && c.Id !=id))
+            {
+                ModelState.AddModelError("CourseName", "Bele adla kurs movcuddur!");
+                courseEditVM.ImageUrl = course.ImageUrl;
+                return View(courseEditVM);
+            }
+
+            if(courseEditVM.Photo != null)
+            {
+                if (!courseEditVM.Photo.CheckImageType())
+                {
+                    ModelState.AddModelError("Photo", "Ancaq Image Elave ele !");
+                    return View();
+                }
+                if (courseEditVM.Photo.CheckImageSize(5))
+                {
+                    ModelState.AddModelError("Photo", "Sekilin hecimi 5 Mgb artiq ola bilmez !");
+                    return View();
+                }
+
+                string fullPath = Path.Combine(_env.WebRootPath, "img", "course", course.ImageUrl);
+
+                if (System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Delete(fullPath);
+                }
+                else
+                {
+                    ModelState.AddModelError("ImageUrl", "Kohne sekil tapilmadi yeniden cehd edin !");
+                    courseEditVM.ImageUrl = course.ImageUrl;
+                    return View(courseEditVM);
+                }
+
+                course.ImageUrl = courseEditVM.Photo.SaveImage(_env,"img","course");
+            }
+            course.CourseName = courseEditVM.CourseName;
+            course.AboutCourse = courseEditVM.AboutCourse;
+            course.Duration = courseEditVM.Duration;
+            course.StartDate = courseEditVM.StartDate;
+            course.CoursePrice = courseEditVM.CoursePrice;
+            course.ClassDuration = courseEditVM.ClassDuration;
+            course.StudentsCount = courseEditVM.StudentsCount;
+            course.Certification = courseEditVM.Certification;
+            course.Description = courseEditVM.Description;
+            course.HowToApply = courseEditVM.HowToApply;
+            course.SkillLevel = courseEditVM.SkillLevel;
+            course.Language = courseEditVM.Language;
+            TempData["Edited"] = "ok";
+
+           
+            _appContext.SaveChanges();
+
 
             return RedirectToAction(nameof(Index));
         }
