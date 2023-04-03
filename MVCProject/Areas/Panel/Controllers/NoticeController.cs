8e0f3a1bf25b9133bef557d7e22d50666c578705
Qurban-93 <qurban231293@gmail.com>
2023-04-03 21:15:13 +0400

    NoticeControlerAdd

--- MVCProject/Areas/Panel/Controllers/NoticeController.cs
+++ MVCProject/Areas/Panel/Controllers/NoticeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCProject.Data;
+using MVCProject.Models;
+using MVCProject.ViewModels.NoticeVMs;
 
 namespace MVCProject.Areas.Panel.Controllers
 {
@@ -13,10 +15,60 @@ namespace MVCProject.Areas.Panel.Controllers
             _webAppContext = webAppContext;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() { return View(_webAppContext.Notices.ToList()); }
+
+        public IActionResult Create() { return View(); }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(NoticeCreateVM noticeCreateVM)
         {
-       
-            return View(_webAppContext.Notices.ToList());
+            if (noticeCreateVM == null) return NotFound();
+            if (!ModelState.IsValid) return View(noticeCreateVM);
+
+            Notice notice = new();
+            notice.PublishDate = DateTime.Now;
+            notice.Content = noticeCreateVM.Content;
+
+            _webAppContext.Notices.Add(notice);
+            _webAppContext.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            Notice notice = _webAppContext.Notices.FirstOrDefault(n => n.Id == id);
+            if (notice == null) return NotFound();
+            return View(notice);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(int? id, NoticeCreateVM noticeCreateVM)
+        {
+            if (id == null || id == 0) return NotFound();
+            if (noticeCreateVM == null) return NotFound();
+            if (!ModelState.IsValid) return View(noticeCreateVM);
+
+            Notice notice = _webAppContext.Notices.FirstOrDefault(n => n.Id == id);
+            if (notice == null) return NotFound();
+
+            notice.Content = noticeCreateVM.Content;
+            _webAppContext.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+            Notice notice = _webAppContext.Notices.FirstOrDefault(n => n.Id == id);
+            if (notice == null) return NotFound();
+
+            _webAppContext.Notices.Remove(notice);
+            _webAppContext.SaveChanges();
+
+            return RedirectToAction("index");
         }
     }
 }
---
+++ MVCProject/ViewModels/NoticeVMs/NoticeCreateVM.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.ViewModels.NoticeVMs
+{
+    public class NoticeCreateVM
+    {
+        [Required]
+        public string? Content { get; set; }       
+    }
+}
